3a3a0e93cb1a4dac3b00660fa1d5d862365b96f0
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-29 11:48:50 +0900

    feat: logback.xml 파일 추가 및 BoardController에 로그 추가

--- src/main/java/org/crud_ex/controller/BoardController.java
+++ src/main/java/org/crud_ex/controller/BoardController.java
@@ -11,13 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/board")
 @RequiredArgsConstructor
-@Log4j
 @Slf4j
 public class BoardController {
 
@@ -26,6 +24,7 @@ public class BoardController {
 	// 목록
 	@GetMapping("/list")
 	public String list(Model model) {
+		log.info("board list...");
 		model.addAttribute("list", boardService.getBoardList());
 		return "board/list";
 	}
@@ -33,12 +32,14 @@ public class BoardController {
 	// 등록 페이지
 	@GetMapping("/register")
 	public String registerForm() {
+		log.info("board register form...");
 		return "board/register";
 	}
 
 	// 등록 처리
 	@PostMapping("/register")
 	public String register(Board board, RedirectAttributes redirectAttributes) {
+		log.info("board register: " + board);
 		boardService.register(board);
 		redirectAttributes.addFlashAttribute("result", board.getBoardId());
 		return "redirect:/board/list";
@@ -47,6 +48,7 @@ public class BoardController {
 	// 상세 조회
 	@GetMapping("/get")
 	public String get(@RequestParam("boardId") Long boardId, Model model) {
+		log.info("board get: " + boardId);
 		model.addAttribute("board", boardService.getBoard(boardId));
 		return "board/get";
 	}
@@ -54,6 +56,7 @@ public class BoardController {
 	// 수정 페이지
 	@GetMapping("/modify")
 	public String modifyForm(@RequestParam("boardId") Long boardId, Model model) {
+		log.info("board modify form: " + boardId);
 		model.addAttribute("board", boardService.getBoard(boardId));
 		return "board/modify";
 	}
@@ -61,6 +64,7 @@ public class BoardController {
 	// 수정 처리
 	@PostMapping("/modify")
 	public String modify(Board board, RedirectAttributes redirectAttributes) {
+		log.info("board modify: " + board);
 		if (boardService.modify(board)) {
 			redirectAttributes.addFlashAttribute("result", "success");
 		}
@@ -70,6 +74,7 @@ public class BoardController {
 	// 삭제 처리
 	@PostMapping("/remove")
 	public String remove(@RequestParam("boardId") Long boardId, RedirectAttributes redirectAttributes) {
+		log.info("board remove: " + boardId);
 		if (boardService.remove(boardId)) {
 			redirectAttributes.addFlashAttribute("result", "success");
 		}
